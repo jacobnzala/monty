35516cc3fd869537e8506b77e8997954d05361fa
jacobnzala <63255452+jacobnzala@users.noreply.github.com>
2022-06-16 17:54:42 +0200

    Update st4.c

--- st4.c
+++ st4.c
@@ -1,30 +1,93 @@
 #include "monty.h"
-/**
- * pstr_stack -main entry
- * Description: prints the string starting at the top of the stack.
- * @top: element at the top of the stack (head)
- * @line_number: line
- * Return: void
- */
-void pstr_stack(stack_t **top, unsigned int line_number)
-{
-	stack_t *tmp;
-	int ascii_num;
 
-	(void)line_number;
-	if (*top == NULL)
+/**
+ * mul - s
+ * @stack: Double linked list
+ * @line_number: File line execution
+ */
+void mul(stack_t **stack, unsigned int line_number)
+{
+	if (!*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%u: can't mul, stack too short\n",
+			line_number);
+		free_all();
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->n = (*stack)->next->n * (*stack)->n;
+	pop(stack, line_number);
+}
+
+
+/**
+ * mod - s
+ * @stack: Double linked list
+ * @line_number: File line execution
+ */
+void mod(stack_t **stack, unsigned int line_number)
+{
+	if (!*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%u: can't mod, stack too short\n",
+			line_number);
+		free_all();
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		free_all();
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->n = (*stack)->next->n % (*stack)->n;
+	pop(stack, line_number);
+}
+
+/**
+ * pchar - s
+ * @stack: Double linked list
+ * @line_number: File line execution
+ */
+void pchar(stack_t **stack, unsigned int line_number)
+{
+	if (!stack || !*stack)
+	{
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
+		free_all();
+		exit(EXIT_FAILURE);
+	}
+	if (isascii((*stack)->n))
+	{
+		printf("%c\n", (*stack)->n);
+		return;
+	}
+	fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
+	free_all();
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * pstr - s
+ * @stack: Double linked list
+ * @line_number: File line execution
+ */
+void pstr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = *stack;
+	(void) line_number;
+
+	if (!stack || !*stack)
 	{
 		putchar('\n');
 		return;
 	}
-	tmp = *top;
-	while (tmp != NULL)
+	while (tmp)
 	{
-		ascii_num = tmp->n;
-
-		if (ascii_num <= 0 || ascii_num > 127)
+		if (tmp->n == 0)
 			break;
-		putchar(ascii_num);
+		if (!isascii((tmp)->n))
+			break;
+		putchar(tmp->n);
 		tmp = tmp->next;
 	}
 	putchar('\n');
